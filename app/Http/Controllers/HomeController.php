e252659eb5680a41ca1a6aab70a2a5235f4a84a3
Ferielle <ferielbhy@yahoo.fr>
2024-09-05 17:45:06 +0100

    test

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        if (Auth::check()) {
+            // Utilisateur connecté
+            return view('home_logged_in');
+        } else {
+            // Utilisateur non connecté
+            return view('home');
+        }    }
+}
